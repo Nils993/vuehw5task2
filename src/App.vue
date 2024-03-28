2b7178293aa4144bc784bed04073d438b32b8b7c
Pavel.S <nils010493@yandex.ru>
2024-03-28 21:46:51 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+
+    <ProductDetails />
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductDetails from "./components/ProductDetails.vue";
+
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ProductDetails, 
+  },
+};
 </script>
 
 <style>
---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>{{ product.name }}</h2>
+    <p>{{ formattedPrice }}</p>
+    <p>{{ product.available ? "Available" : "Out of stock" }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductDetails",
+  props: {},
+  data() {
+    return {
+      product: {
+        name: "Название продукта",
+        price: 99.99,
+        available: true,
+      },
+    };
+  },
+  computed: {
+    formattedPrice() {
+      const price = this.product.price;
+      return `$${price.toFixed(2)}`;
+    },
+  },
+};
+</script>
+<style scoped></style>
